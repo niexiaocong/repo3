06620dce591db4b7a1aae373d491a568752e9cdf
niec <fengyingnc@163.com>
2019-07-23 21:39:23 +0800

    提交hello工程

---
+++ hello/src/com/e3school/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.e3school;
+
+public class HelloWorld {
+
+    public static void main(String args[]){
+        System.out.println("HELLO World");System.out.println("Crlt+Shift+J");
+
+    }
+}
